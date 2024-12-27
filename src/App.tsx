fac0aa6daf8ec677392daf21e928b9c100036600
FabiansDock <fabian20122001@gmail.com>
2024-12-27 16:53:57 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* <LoginPage /> */}
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
