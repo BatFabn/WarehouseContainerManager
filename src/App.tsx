fe1651f7e81f271b734925aa2731d023cc58652d
FabiansDock <fabian20122001@gmail.com>
2024-12-27 17:05:21 +0530

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
 
 const App: React.FC = () => {
--- src/components/StopAllButton.tsx
+++ src/components/StopAllButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Offcanvas } from "bootstrap";
 
 function App() {
