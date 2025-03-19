141d5b3935c481cbb609221d42abb4fb5f2c34e5
BatFabn <romanreigns397@gmail.com>
2025-03-19 22:36:32 +0530

    Initial commit

---
+++ WarehouseContainerBackend/main.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel
+from image_predict import process_image
+from num_predict import process_num_inputs
+from PIL import Image
+import io
+
+app = FastAPI()
+
+
+class Metrics(BaseModel):
+    fruit: float = Form(...),
+    temperature: float = Form(...),
+    humidity: float = Form(...),
+    methane: float = Form(...),
+
+
+def process_data(image: bytes | None, metrics: Metrics | None):
+    fruit_count = "Missing input"
+    fruit_status = "Missing input"
+    if image:
+        img = Image.open(io.BytesIO(image))
+        fruit_count = process_image(img)
+    if metrics and len(metrics) == 4:
+        fruit_status = process_num_inputs(
+            metrics["fruit"], metrics["temperature"], metrics["humidity"], metrics["methane"])
+
+    return {"image": fruit_count, "metrics": fruit_status}
+
+
+@app.post("/predict")
+async def predict(
+    image: Optional[UploadFile] = File(None),
+    metrics: Optional[Metrics] = None
+):
+    image_data = None
+    if image:
+        image_data = await image.read()
+    label = process_data(image_data, metrics)
+    return label
